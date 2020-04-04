c395636199d8d4a1d0bebc71ffa5a090860b7b54
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-04-03 17:14:26 -0700

    implemented Mutation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return (
+      <div>
+        <LinkList/>
+        <CreateLink/>
+      </div>
+    )
   }
 }
 
