4d041aadf801a8a6745c10f6ba4d85161e3d6126
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-04-08 12:38:15 -0700

    Prepare React components for authentication

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink'
+import Login from './Login'
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
 
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
